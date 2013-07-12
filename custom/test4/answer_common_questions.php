faf78953fa9aba7e4821c9bb8abb7ce0b18334d6
nf02 <liuzhipeng197@163.com>
2013-07-12 10:21:11 +0800

    answer_common_questions.php-2

--- custom/test4/answer_common_questions.php
+++ custom/test4/answer_common_questions.php
@@ -22,6 +22,25 @@
   			<li class="active">常见问题与解答</li>
 		</ul>
 	  </div>
+	  <div class='anlie_basicinfo'>	
+	  	<div class='question_form'>
+			<div class='question_title'>
+				1、委托检验书提交以后是否可以撤销？
+			</div>
+			<br>
+			<div class='question_answer'>
+				可以撤销，方法有两种：第一种是通过本系统在网上在线申请撤销；另一种是打电话给我中心前台服务员撤销。
+			</div>
+	  	</div>
+		<div class='question_form'>                                                                                                                                                                   		      <div class='question_title'>
+                                2、报告发布后，报告的信息是否可以修改？
+                        </div>
+                        <br>
+                        <div class='question_answer'>                                                                                                                                                 
+                        	报告发布后，报告内容的修改需根据具体情况而定，如简单的错别字修改可以，公司信息修改一般不允许。详情请咨询010-89055269。
+			</div>                                                                                                                                                                        
+                </div> 
+	  </div>
   <div class="bottom">
    <?php
 	iframe_bottom();
@@ -32,23 +51,3 @@
 </body>
 </html>
 
-<script language="javascript">
-
-
-function mysubmit(){
-		
-		//var obj_val= $('#test_obj').val();
-		var obj_val=document.getElementById('test_obj').value;
-		//alert(obj_val);
-		if(obj_val==''){
-			alert("请选择检测对象");
-		}else{
-			//$('#h_test_obj').val()=obj_val;
-			document.form_sel_obj.submit();
-		}
-		
-	}
-
-	
-
-</script>
